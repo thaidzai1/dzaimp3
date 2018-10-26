bc0eb1e7070ef1a48715087894272b8d84553f84
thaiduong <ntduong28597@gmail.com>
2018-10-26 21:42:33 +0700

    test deploy

--- index.js
+++ index.js
@@ -9,6 +9,14 @@ mongoose.connect(keys.mongoURI, () => {
 
 const app = express();
 
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static('client/dist'));
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+  })
+}
+
 const _PORT = process.env.PORT || 5000;
 app.listen(_PORT, () => {
   console.log('listen port', _PORT);

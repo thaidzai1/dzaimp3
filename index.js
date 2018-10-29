bc6e53f2aef36afde7882efc07f1af1a9a832427
thaiduong <ntduong28597@gmail.com>
2018-10-29 20:19:49 +0700

    make it better

--- index.js
+++ index.js
@@ -43,6 +43,10 @@ if(process.env.NODE_ENV === 'production'){
 }
 
 require('./routes')(app);
+app.get('/auth/logout', (req, res) => {
+  req.logout();
+  res.redirect('/');
+})
 
 //create port and server
 const _PORT = process.env.PORT || 5000;

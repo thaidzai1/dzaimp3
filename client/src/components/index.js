7fb8f59ac95e304d5a3aaf0375298f36531acbd6
thaiduong <ntduong28597@gmail.com>
2018-11-20 22:28:53 +0700

    fixed

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -3,13 +3,11 @@ import NavBar from './NavBar'
 import FloatMusic from './FloatMusic'
 import HomePage from './HomePage'
 import Song from './Song'
-import * as Pages from './Pages'
 
 export {
   AuthPage,
   NavBar,
   FloatMusic,
   HomePage,
-  Song,
-  Pages
+  Song
 }
